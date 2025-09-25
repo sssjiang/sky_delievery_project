da80b63c2cee388ecf5f78231ff6e029ddfb18d4
zelin <zelinjiang@cuhk.edu.hk>
2025-09-25 16:52:31 +0800

    新增员工开发

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+import com.sky.dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+    void save(EmployeeDTO employeeDTO);
+
+}
