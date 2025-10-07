213e942ea1c37fa336caa073aa76e955e665c0ff
zelin <zelinjiang@cuhk.edu.hk>
2025-10-07 17:53:27 +0800

    店铺营业状态设置开发1.redis配置，2.接口文档分类设置，3，店铺设置

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,38 @@
+package com.sky.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import io.swagger.annotations.Api;
+import com.sky.result.Result;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+    public static final String KEY = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+   @PutMapping("/{status}")
+   @ApiOperation("设置店铺状态")
+   public Result setStatus(@PathVariable Integer status) {
+    log.info("设置店铺状态: {}", status);
+    redisTemplate.opsForValue().set(KEY, status);
+    return Result.success("店铺状态设置成功");
+   }
+   @GetMapping("/status")
+   @ApiOperation("获取店铺状态")
+   public Result<Integer> getStatus() {
+    Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+    log.info("获取店铺状态: {}", status);
+    return Result.success(status);
+   }
+    
+}
